411244367bb32a60a13f569e5fe41f3ea2367462
Shubham Shahaji Dhanvate <93417980+shubhamssd@users.noreply.github.com>
2025-02-16 20:12:19 +0530

    Update OrderService.java

--- order-service/src/main/java/com/axl/microservice/order/service/OrderService.java
+++ order-service/src/main/java/com/axl/microservice/order/service/OrderService.java
@@ -1,9 +1,11 @@
 package com.axl.microservice.order.service;
 
+import com.axl.microservice.order.client.InventoryClient;
 import com.axl.microservice.order.dto.OrderRequest;
 import com.axl.microservice.order.model.Order;
 import com.axl.microservice.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.var;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -13,17 +15,24 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
 
     public String placeOrder(OrderRequest request, String orderPlacedSuccessfully) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(request.price());
-        order.setSkuCode(request.skuCode());
-        order.setQuantity(request.quantity());
+        var isProductInStock = inventoryClient.isInStock(request.skuCode(), request.quantity());
 
-        Order placedOrder = orderRepository.save(order);
-        return placedOrder + "orders placed";
+        if (isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(request.price());
+            order.setSkuCode(request.skuCode());
+            order.setQuantity(request.quantity());
 
+            Order placedOrder = orderRepository.save(order);
+            return placedOrder + "orders placed";
+
+        }else {
+            throw new RuntimeException("Product with SkuCode " + request.skuCode() + "is not in stock" );
+        }
     }
 }
